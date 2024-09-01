95a75c4e08278a3511d28e341bad9193be1caf8a
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2024-09-01 08:55:31 +0700

    Update my Portfolio

--- src/components/Award.jsx
+++ src/components/Award.jsx
@@ -1,52 +1,48 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
-import { fadeIn, textVariant } from '../utils/motion';
 
-import olympic from '../assets/certificate/olympic.jpg'
-import startup from '../assets/certificate/startup.jpg'
-import ujikom from '../assets/certificate/ujikom.jpg'
-import topujikom from '../assets/certificate/topujikom.jpg'
+import olympic from '../assets/certificate/olympic.jpg';
+import startup from '../assets/certificate/startup.jpg';
+import ujikom from '../assets/certificate/ujikom.jpg';
+import topujikom from '../assets/certificate/topujikom.jpg';
 
 const Award = () => {
   const awards = [
     {
       title: "First Overall Champion in Software Engineering at SMK Assalaam Bandung",
       year: "2024",
-      image: ujikom, 
+      image: ujikom,
     },
     {
       title: "First Place in the Expertise Competency Exam of SMK Assalaam Bandung",
       year: "2024",
-      image: topujikom, 
+      image: topujikom,
     },
     {
       title: "Active Participation in the TechUpdate Online Vol. 51 x Roadshow Gerakan Nasional 1000 Startup Digital",
       year: "2023",
-      image: startup, 
+      image: startup,
     },
     {
       title: "Golden Winner in National English and Science Olympic Global House at Province Level with a score between 957 – 995",
       year: "2022",
-      image: olympic, 
+      image: olympic,
     },
   ];
 
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>Recognitions and Achievements</p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>Honors & Awards</h2>
-      </motion.div>
+      <div className="text-center">
+        <p className={`${styles.sectionSubText}`}>Recognitions and Achievements</p>
+        <h2 className={`${styles.sectionHeadText}`}>Honors & Awards</h2>
+      </div>
 
       <div className="mt-10 flex flex-col lg:flex-row lg:flex-wrap justify-center items-center gap-8">
         {awards.map((award, index) => (
-          <motion.div
+          <div
             key={index}
-            variants={fadeIn("up", "tween", index * 0.3, 1)}
-            className="max-w-sm w-full bg-tertiary p-6 rounded-lg shadow-lg transform transition duration-500 hover:scale-105"
+            className="max-w-sm w-full bg-tertiary p-6 rounded-lg shadow-lg transition-transform duration-500 hover:scale-105"
           >
             <div className="flex flex-col items-center">
               <h3 className="text-white text-lg font-extrabold text-center">{award.title}</h3>
@@ -57,11 +53,11 @@ const Award = () => {
               />
               <p className="text-gray-300 text-sm mt-4">{award.year}</p>
             </div>
-          </motion.div>
+          </div>
         ))}
       </div>
     </>
   );
-}
+};
 
 export default SectionWrapper(Award, "");
